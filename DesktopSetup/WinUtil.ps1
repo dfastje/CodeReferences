30a3bd49d64777a1f1a17e4404976d740944b826
dfastje <dfastjework@protonmail.com>
2024-12-10 12:01:25 -0500

    Adding winutil to setup notes

---
+++ DesktopSetup/WinUtil.ps1
@@ -0,0 +1,6 @@
+#Utility to remove telemetry from windows:
+#https://github.com/christitustech/winutil
+#https://www.youtube.com/watch?v=5_AaHXrelTE
+
+#Needs admin rights in powershell
+irm "https://christitus.com/win" | iex
