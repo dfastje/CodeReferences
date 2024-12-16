0eb0bf89212be17e41fb3077cb93f7ed7b2506b3
dfastje <dfastjework@protonmail.com>
2024-12-16 17:49:45 -0500

    adding portainer and cloudflaretunnel docker sections. not finished yet, but working

---
+++ DesktopSetup/docker-containers/CloudflareTunnel.sh
@@ -0,0 +1,5 @@
+#Cloudflare Tunnel
+
+#docker run cloudflare/cloudflared:latest tunnel --no-autoupdate run --token <token>
+
+
---
+++ DesktopSetup/docker-containers/portainer.sh
@@ -0,0 +1,7 @@
+#Installing Portainer for the first time on a machine
+#https://docs.portainer.io/start/install-ce
+# https://docs.portainer.io/start/install-ce/server/docker
+#   https://docs.portainer.io/start/install-ce/server/docker/linux
+
+
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
