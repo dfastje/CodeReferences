597e20af2d60272483362b3b60dcd7177153fbbc
dfastje <dfastjework@protonmail.com>
2024-12-18 00:57:30 -0500

    minor changes to test

--- DesktopSetup/docker-containers/Fresh-rss.sh
+++ DesktopSetup/docker-containers/Fresh-rss.sh
@@ -1,8 +1,11 @@
-sudo docker run -d --restart unless-stopped --log-opt max-size=10m \
-  -p 8020:80 \
-  -e TZ=America/New_York \
-  -e 'CRON_MIN=1,31' \
-  -v freshrss_data:/var/www/FreshRSS/data \
-  -v freshrss_extensions:/var/www/FreshRSS/extensions \
-  --name freshrss \
-  freshrss/freshrss
+#sudo docker run -d --restart unless-stopped --log-opt max-size=10m \
+#  -p 8020:80 \
+#  -e TZ=America/New_York \
+#  -e 'CRON_MIN=1,31' \
+#  -v freshrss_data:/var/www/FreshRSS/data \
+#  -v freshrss_extensions:/var/www/FreshRSS/extensions \
+#  --name freshrss \
+#  freshrss/freshrss
+
+
+docker compose -f freshrss-compose.yml up -d
