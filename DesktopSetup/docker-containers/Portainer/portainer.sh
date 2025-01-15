c2871958f482ed05476179fffada07004633910d
dfastje <dfastjework@protonmail.com>
2025-01-15 16:35:27 +0000

    Adding compose down to the portainer script

--- DesktopSetup/docker-containers/Portainer/portainer.sh
+++ DesktopSetup/docker-containers/Portainer/portainer.sh
@@ -7,4 +7,5 @@
 
 #sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
-docker compose up -d
+sudo docker compose up -d
+#sudo docker compose down
