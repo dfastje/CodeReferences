640f78c4cdbce8a9631ffc542a6d7d28036a9ab7
dfastje <dfastjework@protonmail.com>
2025-01-27 18:42:54 -0500

    pushing proxmox notes

--- proxmox/Proxmox.sh
+++ proxmox/Proxmox.sh
@@ -3,4 +3,14 @@
 #https://forum.proxmox.com/threads/pci-gpu-passthrough-on-proxmox-ve-8-installation-and-configuration.130218/
 
 #GUI access issue:
-#Solution was to modify the vlan mask to match the vlan (like I should have input correctly during the install)
+#Issue: rebooted proxmox after modifying the vlan mask to match network (with typo)
+#Solution: modify the vlan mask to match the vlan in the interfaces file
+#   /etc/network/interfaces (traditional debian style)
+#   (like I should have input correctly during the install)
+
+#GUI access issue 2:
+#Issue: removed nic1&2 to put nic1&2 in desktop with PCI pass through support.
+#   Verified access over Static IP assigned to remaining nic0 in system
+#   (to support VM network isolation)
+#Failed Solution: update /etc/hosts to reflect Static IP address of remaining nic0
+#Solution: remove the removed nic1&2 from /etc/network/interfaces
