b943768ff1f5c56734195fc6d96f9324ece6019a
dfastje <dfastjework@protonmail.com>
2025-01-31 11:46:45 -0500

    updating for vm transfer

--- docker-containers/CloudflareTunnel/CloudflareTunnel.sh
+++ docker-containers/CloudflareTunnel/CloudflareTunnel.sh
@@ -6,6 +6,6 @@
 #Couldn't get macvlan to work due to limitations in ubiquiti's unifi
 # (I would need to add a second nic to the computer ... probably will in the future)
 
-#export TUNNEL_TOKEN=
+export TUNNEL_TOKEN=eyJhIjoiMjBiNzE0ODc4MzJhYmNmMjNiNzNhZTZlNDRlNTBiN2QiLCJ0IjoiNmFhZWQyZWMtZjc1Yi00ZDg2LThlNDUtNzcwMjJhM2RjMTg2IiwicyI6IllXWXhNbVUzWmpjdE5EbGhaUzAwTmpneUxUZ3pZakl0WXpOaFpqUmxPREF3T0RndyJ9
 
 sudo -E docker compose up -d
