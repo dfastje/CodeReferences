7a2de36469e796725ae060820c48ad0e159b189a
dfastje <dfastjework@protonmail.com>
2025-04-10 23:58:15 -0400

    updating syncthing to use env file

--- docker-containers/Syncthing/Syncthing.sh
+++ docker-containers/Syncthing/Syncthing.sh
@@ -2,10 +2,11 @@
 # https://hub.docker.com/r/linuxserver/syncthing
 # https://github.com/linuxserver/docker-syncthing?tab=readme-ov-file
 
+#Params:
+#ENV_DATA1_PATH: path to media files with source of truth elsewhere
+#ENV_DATA2_PATH: path to docker config volumes to backup
+#ENV_DATA3_PATH: path to syncthing files with source of truth on this machine
 
 sudo docker compose down
 sudo docker compose pull
-
-ST_DATA1_PATH=/media/ubuntuprod/HDD3/syncthing \
-ST_DATA1_PATH=/home/ubuntuprod/Syncthing/docker \
 sudo docker compose up -d
