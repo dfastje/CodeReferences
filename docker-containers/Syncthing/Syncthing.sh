3fd863eb0388994ccb100257cddf2926c6949d8f
dfastje <dfastjework@protonmail.com>
2025-04-11 00:08:00 -0400

    changing the syncthing folders

--- docker-containers/Syncthing/Syncthing.sh
+++ docker-containers/Syncthing/Syncthing.sh
@@ -3,9 +3,9 @@
 # https://github.com/linuxserver/docker-syncthing?tab=readme-ov-file
 
 #Params:
-#ENV_DATA1_PATH: path to media files with source of truth elsewhere
-#ENV_DATA2_PATH: path to docker config volumes to backup
-#ENV_DATA3_PATH: path to syncthing files with source of truth on this machine
+#SYNCTHING: files owned by other machines
+#SYNCTHING_LOCAL: files owned by this machine
+
 
 sudo docker compose down
 sudo docker compose pull
