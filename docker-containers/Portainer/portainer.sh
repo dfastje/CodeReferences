02488c66029ce8392be214bedb3c1c60fb59d74c
dfastje <dfastjework@protonmail.com>
2025-05-10 20:24:49 -0400

    modifying portainer script to make updates easier

--- docker-containers/Portainer/portainer.sh
+++ docker-containers/Portainer/portainer.sh
@@ -7,5 +7,6 @@
 
 #sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
+sudo docker compose down
+sudo docker compose pull
 sudo docker compose up -d
-#sudo docker compose down
