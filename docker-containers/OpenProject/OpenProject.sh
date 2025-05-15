df4a31895844138846dcff5434a976271e4742c5
dfastje <dfastjework@protonmail.com>
2025-05-15 16:14:15 -0400

    Adding OpenProject docker compose first upload

---
+++ docker-containers/OpenProject/OpenProject.sh
@@ -0,0 +1,7 @@
+#https://www.openproject.org/docs/installation-and-operations/installation/docker/#installation-using-docker-compose
+#https://hub.docker.com/r/openproject/openproject
+
+
+#sudo docker compose down
+#sudo docker compose pull
+sudo docker compose up -d
