4ac5b989c5f709b5adf2813f7d4519b020558b06
dfastje <dfastjework@protonmail.com>
2025-06-19 16:09:09 -0400

    modifying smb script (still not working, but close enough for now to manually run)

--- proxmox/VMs/ubuntu/ubuntu_smb.sh
+++ proxmox/VMs/ubuntu/ubuntu_smb.sh
@@ -1,6 +1,6 @@
 #SMB setup
 NAS_IP="192.168.2.171"
-NAS_DIR="/media"
+NAS_DIR="media"
 MNT_DIR="/mnt/${NAS_DIR}"
 
 #-----------------------------------------------------------------------
@@ -36,11 +36,14 @@ if [ ! -f "$CREDS_FILE" ] || [ ! -r "$CREDS_FILE" ]; then
     echo "Error: Cannot access credentials file."
     exit 1
 fi
-read -p "When you've finished inputing the creds into ${CREDS_FILE}, press Enter..."
+echo "input the creds into ${CREDS_FILE}"
+if grep -q "your_smb_password" ${CREDS_FILE}; then
+  exit 1
+fi
 #-----------------------------------------------------------------------
 #Edit /etc/fstab for mounting
 if grep -q " ${MNT_DIR} " /etc/fstab; then
   echo "Mount point already exists in /etc/fstab."
 else
-  echo "//${NAS_IP}/${NAS_DIR}   ${MNT_DIR}   cifs   credentials=/etc/smb-credentials,iocharset=utf8,uid=1000,gid=1000,file_mode=0770,dir_mode=0770,nofail  0  0" >> /
+  echo "//${NAS_IP}/${NAS_DIR}   ${MNT_DIR}   cifs   credentials=/etc/smb-credentials,iocharset=utf8,uid=1000,gid=1000,file_mode=0770,dir_mode=0770,nofail  0  0" >> /etc/fstab
 fi
