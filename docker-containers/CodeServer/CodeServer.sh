5d35e00589c979968c1f88b1e468b2ed37e4a577
dfastje <dfastjework@protonmail.com>
2025-10-01 18:56:01 -0400

    adding codeserver code for deployment testing

--- docker-containers/CodeServer/CodeServer.sh
+++ docker-containers/CodeServer/CodeServer.sh
@@ -1,3 +1,5 @@
+# https://hub.docker.com/r/linuxserver/code-server
+
 sudo docker compose down
 sudo docker compose pull
 sudo docker compose up -d
---
+++ docker-containers/Postgres/Postgres.sh
@@ -0,0 +1,5 @@
+#https://hub.docker.com/_/postgres/
+#http://localhost:8080
+sudo docker compose down
+sudo docker compose pull
+sudo docker compose up -d
